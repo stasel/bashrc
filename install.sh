d565cfa90e32ee39c1ef0d9f14e4b31fda25d5d1
Stasel <2033301+stasel@users.noreply.github.com>
2019-07-13 13:45:46 +0200

    Fixed homepath

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 function addSourceIfNeeded() {
     local rcpath=$1
-    if [ -f $rcpath ] && ! grep -q 'source .stasel.sh' $rcpath ; then 
-        echo $'\n# stasel additions\nsource .stasel.sh\n' >> $rcpath
+    if [ -f $rcpath ] && ! grep -q 'source ~/.stasel.sh' $rcpath ; then 
+        echo $'\n# stasel additions\nsource ~/.stasel.sh\n' >> $rcpath
         echo "Installed aliases to $rcpath"
     fi
 }
